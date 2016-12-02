1dc7bd86f8b5d1da0660ea909cd14f899806765f
johnhovanec <jahovanec@gmail.com>
2016-12-02 13:01:01 -0500

    Add subtotal calculation in checkout

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -11,7 +11,24 @@ const Cart = require('../models/Cart.js');
 // /* GET cart by userID. */
  exports.index = (req, res, next) => {
  Cart.findOne({"userID": req.params.userID }, (err, cart) => {
-    if (err) { return next(err); }
+    if (err) { 
+      return next(err); 
+    }
+
+    cart.subTotal = 0;      // Let's reset it each time
+    // loop through items in cart to calculate subtotal, total, etc.
+    for (var i = 0; i < cart.items.length; i++) {
+      console.log(" prices in cart = " + cart.items[i].price);
+      cart.subTotal += cart.items[i].price * cart.items[i].quantity;
+    }
+    console.log("Subtotal = " + cart.subTotal);
+    //cart.subTotal = parseFloat(cart.subTotal) + (parseFloat(req.body.price)* parseInt(req.body.quantity));   // Need to parseFloat to avoid validation error
+    cart.taxAmount = cart.subTotal * cart.taxRate;                    // Apply MD state tax
+    if (cart.subTotal > 50.00 ) {                                                     // Shipping over $50 is free, otherwise it's a flate rate   existingCart.shippingRate = 0.00;
+    } else {
+      cart.shippingAmount = 7.95;
+    }
+    cart.total += cart.subTotal + cart.taxAmount + cart.shippingAmount;    // Calculate total
     console.log("In checkout: userID = " + req.body.userID)
     res.render('checkout', { carts: cart });
   });
